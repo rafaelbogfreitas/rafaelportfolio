55271b481457c2ea95fb07ce4aebce343836a217
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-05-25 09:46:39 -0300

    Include portfolio project translation

---
+++ precache-manifest.be52118858ff8b726d9738f2f555047f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5353198abe448bbc3823ffc33d940be6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e6f1931926898ea8307",
+    "url": "/static/css/main.3f60dfda.chunk.css"
+  },
+  {
+    "revision": "690334a51a9b3be29ca7",
+    "url": "/static/js/2.b71f755b.chunk.js"
+  },
+  {
+    "revision": "2e6f1931926898ea8307",
+    "url": "/static/js/main.0c5e49dc.chunk.js"
+  },
+  {
+    "revision": "17cfbdb56e6454e14c0c",
+    "url": "/static/js/runtime-main.b9136a75.js"
+  },
+  {
+    "revision": "0cee329b9d684dd7f441a13d6bcd3ecd",
+    "url": "/static/media/arturasgrimalis.0cee329b.png"
+  },
+  {
+    "revision": "928805b4f3cf4db7ef1f61e5f3aeb8ef",
+    "url": "/static/media/formproject.928805b4.png"
+  },
+  {
+    "revision": "c7d4fcf13a5788d5d4f648868471fe4c",
+    "url": "/static/media/jWebsite.c7d4fcf1.png"
+  },
+  {
+    "revision": "dd779872e438f77ef484302599b380cb",
+    "url": "/static/media/productLanding.dd779872.png"
+  },
+  {
+    "revision": "97a2469e75009ebee22effe878e9ded9",
+    "url": "/static/media/rafael.97a2469e.svg"
+  },
+  {
+    "revision": "019fef5dbe54f13f0bafc3965aedf776",
+    "url": "/static/media/technicaldoc.019fef5d.png"
+  },
+  {
+    "revision": "5f43b68f5cc7620c7cf51d8fbe63bc83",
+    "url": "/static/media/tributePage.5f43b68f.png"
+  },
+  {
+    "revision": "9c127f2c14d47a26c6e6f0252c3bf5b0",
+    "url": "/static/media/webPortfolio.9c127f2c.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.23d9af17e5286c13a0470c60dd4da72d.js"
+  "/precache-manifest.be52118858ff8b726d9738f2f555047f.js"
 );
 
 self.addEventListener('message', (event) => {
