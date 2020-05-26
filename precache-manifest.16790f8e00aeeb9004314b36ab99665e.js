9465c53b105e5c055a493823661d7aaa31946491
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-05-26 20:39:30 -0300

    Add new project and translate it

---
+++ precache-manifest.16790f8e00aeeb9004314b36ab99665e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b49f04c0350a45e3ca6f4b181eb9cc7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b81eecd00c7764be581",
+    "url": "/static/css/main.3f60dfda.chunk.css"
+  },
+  {
+    "revision": "487f8d1eedd8fd817049",
+    "url": "/static/js/2.f90f9dbf.chunk.js"
+  },
+  {
+    "revision": "2b81eecd00c7764be581",
+    "url": "/static/js/main.db343a05.chunk.js"
+  },
+  {
+    "revision": "17cfbdb56e6454e14c0c",
+    "url": "/static/js/runtime-main.b9136a75.js"
+  },
+  {
+    "revision": "928805b4f3cf4db7ef1f61e5f3aeb8ef",
+    "url": "/static/media/formproject.928805b4.png"
+  },
+  {
+    "revision": "dd779872e438f77ef484302599b380cb",
+    "url": "/static/media/productLanding.dd779872.png"
+  },
+  {
+    "revision": "97a2469e75009ebee22effe878e9ded9",
+    "url": "/static/media/rafael.97a2469e.svg"
+  },
+  {
+    "revision": "019fef5dbe54f13f0bafc3965aedf776",
+    "url": "/static/media/technicaldoc.019fef5d.png"
+  },
+  {
+    "revision": "5f43b68f5cc7620c7cf51d8fbe63bc83",
+    "url": "/static/media/tributePage.5f43b68f.png"
+  }
+]);
